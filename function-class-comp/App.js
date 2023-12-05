6962b4687f2a6973e99cf29fde28264f31a72f57
NITHINKR06 <146012853+NITHINKR06@users.noreply.github.com>
2023-12-04 22:10:48 -1200

    Add files via upload

---
+++ function-class-comp/App.js
@@ -0,0 +1,27 @@
+// src/App.js
+import React from 'react';
+
+function App () {
+  return (
+    <>
+    <h1>This is function components</h1>
+    </>
+  );
+};
+
+export default App;
+
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>This is function components</h1>
+      </>
+    );
+  }
+}
+
+export default App;
+
