25802806da71e0c46eaa1a3c471d6d616f53ec0e
NITHINKR06 <146012853+NITHINKR06@users.noreply.github.com>
2023-12-04 21:53:54 -1200

    Add files via upload

---
+++ react-router/App.js
@@ -0,0 +1,38 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Home from './comp/Home';
+import About from './comp/About';
+import Contact from './comp/Contact';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <hr />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
