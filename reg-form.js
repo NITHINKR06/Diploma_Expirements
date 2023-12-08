4d96b588a9e29583cdee596e005d293d605d5008
NITHINKR06 <146012853+NITHINKR06@users.noreply.github.com>
2023-12-08 09:09:46 +0530

    Update reg-form.js

--- reg-form.js
+++ reg-form.js
@@ -1,150 +1,103 @@
-import React, { useState } from 'react';
 
-function App() {
+import React, { useState, useRef } from 'react';
+import './App.css';
+
+const App = () => {
   const [formData, setFormData] = useState({
     name: '',
+    regNo: '',
     address: '',
-    state: '',
     gender: '',
-    qualification: [],
+    semester: '',
   });
 
-  const [submittedData, setSubmittedData] = useState(null);
+  const [displaySubmission, setDisplaySubmission] = useState(false);
 
-  const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
+  const fileInputRef = useRef(null);
 
-    if (type === 'checkbox') {
-      if (checked) {
-        setFormData((prevData) => ({
-          ...prevData,
-          qualification: [...prevData.qualification, value],
-        }));
-      } else {
-        setFormData((prevData) => ({
-          ...prevData,
-          qualification: prevData.qualification.filter((qual) => qual !== value),
-        }));
-      }
-    } else {
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: value,
-      }));
-    }
+  const handleInputChange = (e) => {
+    const { name, value, type } = e.target;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === 'file' ? fileInputRef.current.files[0] : value,
+    }));
   };
 
   const handleSubmit = () => {
-    const { name, address, state, gender, qualification } = formData;
-
-    setSubmittedData({
-      name,
-      address,
-      state,
-      gender,
-      qualification,
-    });
+    setDisplaySubmission(true);
   };
 
   return (
     <div>
+      <div>
+        <center>
+          <h1>SDM Polytechnic, Ujire, Belthangady Tq</h1>
+          <h4>Affiliated to AICTE, New Delhi, Registered by DTE</h4>
+        </center>
+      </div>
+
       <h1 align="center">Registration Form</h1>
-      <form>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Enter your name..."
-            value={formData.name}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="address">Address</label>
-          <textarea
-            rows="5"
-            cols="50"
-            name="address"
-            id="address"
-            placeholder="Enter your address..."
-            value={formData.address}
-            onChange={handleChange}
-          ></textarea>
-        </div>
-
-        <div>
-          <label htmlFor="state">State</label>
-          <select
-            name="state"
-            id="state"
-            value={formData.state}
-            onChange={handleChange}
-          >
-            <option value="KL">Kerala</option>
-            <option value="KA">Karnataka</option>
-            <option value="MH">Maharashtra</option>
-            <option value="GU">Gujarat</option>
-            <option value="AP">Andhra Pradesh</option>
-          </select>
-        </div>
-
-        <div>
-          <label>Gender</label>
-          <input
-            type="radio"
-            name="gender"
-            value="m"
-            checked={formData.gender === 'm'}
-            onChange={handleChange}
-          />
-          Male
-          <input
-            type="radio"
-            name="gender"
-            value="f"
-            checked={formData.gender === 'f'}
-            onChange={handleChange}
-          />
-          Female
-        </div>
-
-        <div>
-          <label>Qualification</label>
-          <input
-            type="checkbox"
-            value="SSLC"
-            name="qualification"
-            checked={formData.qualification.includes('SSLC')}
-            onChange={handleChange}
-          />
-          SSLC
-          <input type="checkbox" value="PUC" name="n6" id="i7" />PUC
-            <input type="checkbox" value="DIPLOMA" name="n7" id="i8" />DIPLOMA 
-            <input type="checkbox" value="BE" name="n8" id="i9" />BE
-        
-        </div>
-
-        
-
-        <button type="button" onClick={handleSubmit}>
-          Submit
-        </button>
-      </form>
-
-      {submittedData && (
+      {displaySubmission ? (
         <div className="display-section">
-          <h2>Thank you for your submission, {submittedData.name}!</h2>
-          <p>Address: {submittedData.address}</p>
-          <p>State: {submittedData.state}</p>
-          <p>Gender: {submittedData.gender}</p>
-          <p>Qualification: {submittedData.qualification.join(', ')}</p>
+          <h2>Thank you for your submission, {formData.name}!</h2>
+          <p>Registration Number: {formData.regNo}</p>
+          <p>Address: {formData.address}</p>
+          <p>Gender: {formData.gender}</p>
+          <p>Semester: {formData.semester}</p>
+          {formData.photo && (
+            <div>
+              <p>Photo:</p>
+              <img src={URL.createObjectURL(formData.photo)} alt="Uploaded" style={{ maxWidth: '100%', maxHeight: '200px' }} />
+            </div>
+          )}
         </div>
+      ) : (
+        <form>
+          
+          <div>
+           <label htmlFor="i1">Name</label>
+           <input type="text" size="15" name="name" id="i1" placeholder="Enter your name..." onChange={handleInputChange} />
+         </div><br />
+
+         <div>
+           <label htmlFor="regNo">Reg No:</label>
+           <input type="text" name="regNo" id="regNo" placeholder="Enter registration number..." onChange={handleInputChange} />
+         </div><br />
+
+         <div>
+           <label htmlFor="i2">Address</label>
+           <textarea rows="5" cols="50" name="address" id="i2" onChange={handleInputChange}></textarea><br />
+         </div><br />
+
+         <div>
+           <label>Gender</label>
+           <input type="radio" name="gender" value="m" onChange={handleInputChange} /> Male 
+           <input type="radio" name="gender" value="f" onChange={handleInputChange} /> Female<br /><br />
+         </div><br />
+
+          <div id="qu">
+            <label>Semester</label>
+            {[1, 2, 3, 4, 5, 6].map((semester) => (
+              <label key={semester}>
+                <input type="radio" value={semester} name="semester" onChange={handleInputChange} /> {semester}
+              </label>
+            ))}
+          </div><br />
+
+          <div>
+            <label htmlFor="i10">Upload Your Photo</label>
+            <input type="file" name="photo" id="i10" ref={fileInputRef} onChange={handleInputChange} />
+          </div><br />
+
+          <button type="button" onClick={handleSubmit}>
+            Submit
+          </button>
+        </form>
       )}
     </div>
   );
-}
+};
 
 export default App;
+
