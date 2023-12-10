948b735552b84b117df44ad69b7f52656fc50481
NITHINR06 <146012853+NITHINR06@users.noreply.github.com>
2023-12-10 09:50:38 +0530

    Update App.js

--- controlled-uncontrolled/App.js
+++ controlled-uncontrolled/App.js
@@ -1,63 +1,76 @@
-
-import React, { useRef } from "react";
-import "./App.css";
- 
-function App() {
-    const inputRef = useRef(null);
- 
-    function handleSubmit() {
-        alert(`Name: ${inputRef.current.value}`);
-    }
- 
-    return (
-        <div className="App">
-            <h1 className="geeks">GeeksForGeeks</h1>
-            <h3>Uncontrolled Component</h3>
-            <form onSubmit={handleSubmit}>
-                <label>Name :</label>
-                <input
-                    type="text"
-                    name="name"
-                    ref={inputRef}
-                />
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    );
-}
- 
-export default App;
-
-
+// Controller Comp
 import { useState } from "react";
-import "./App.css";
- 
-function App() {
-    const [name, setName] = useState("");
- 
-    function handleSubmit() {
-        alert(`Name: ${name}`);
-    }
- 
-    return (
-        <div className="App">
-            <h1 className="geeks">GeeksForGeeks</h1>
-            <h3>Controlled Component</h3>
-            <form onSubmit={handleSubmit}>
-                <label>Name:</label>
-                <input
-                    name="name"
-                    value={name}
-                    onChange={(e) =>
-                        setName(e.target.value)
-                    }
-                />
-                <button type="submit">Submit</button>
-            </form>
+
+export default function App() {
+  const [name, setName] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    document.getElementById("color").style.backgroundColor = name;
+    alert(`Name: ${name}`);
+  }
+
+  return (
+    <div>
+      <center>
+        <div style={{ margin: "200px" }}>
+          <h1 style={{ backgroundColor: "transparent" ,width:'50%'}} id="color">
+            Controlled Component
+          </h1>
+          <h3>UseState</h3>
+          <form onSubmit={handleSubmit}>
+            <label>Name:</label>
+            <input
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button type="submit" style={{background:'black' ,color:'white' ,borderRadius:'10%', marginLeft:'10px',width:'100px',height:'25px'}}>Submit</button>
+          </form>
         </div>
-    );
+      </center>
+    </div>
+  );
 }
+
+
+<!--      111111      -->
+ <!--      1111      -->
+  <!--      11     -->
+
+
  
+// Uncontroller Comp
+import React, { useRef } from "react";
+
+function App() {
+  const inputRef = useRef(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const color = inputRef.current.value;
+    document.getElementById("color").style.backgroundColor = color;
+    alert(`Color: ${color}`);
+  }
+
+  return (
+    <div>
+      <center>
+        <div style={{ margin: "200px" }}>
+          <h1 style={{ backgroundColor: "transparent", width:'50%'}} id="color">
+            Uncontrolled Component
+          </h1>
+          <h3>UseRef</h3>
+          <form onSubmit={handleSubmit}>
+            <label>Enter Fav Color :</label>
+            <input type="text" ref={inputRef} />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </center>
+    </div>
+  );
+}
+
 export default App;
-
-
